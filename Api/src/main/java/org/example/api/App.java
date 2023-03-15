f84305daa0364457aee0172a9af92e5a3c2a8116
abboskhan <abbosxon28012004@gmail.com>
2023-03-15 17:41:55 +0500

    last time changed

--- Api/src/main/java/org/example/api/App.java
+++ Api/src/main/java/org/example/api/App.java
@@ -4,6 +4,6 @@ import org.example.core.Utils;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+        System.out.println("Answer is : " + Utils.isAllPositiveNumbers("12", "79"));
     }
 }

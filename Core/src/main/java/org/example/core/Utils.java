e11d4af27a28f8075ffd566e110aa40a1ee7a324
Abbosxon Abdujalilov <80406837+Abbosxon2004@users.noreply.github.com>
2023-03-30 11:03:10 +0500

    success

--- Core/src/main/java/org/example/core/Utils.java
+++ Core/src/main/java/org/example/core/Utils.java
@@ -2,8 +2,6 @@ package org.example.core;
 
 import org.example.StringUtils;
 
-import java.util.List;
-
 public class Utils {
     public static boolean isAllPositiveNumbers(String... args) {
         for (String arg : args) {
